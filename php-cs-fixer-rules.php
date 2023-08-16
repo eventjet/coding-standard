1fe223ba7490cfffa85e8fb6795c42bd28c3f3a0
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2023-08-16 11:46:27 +0200

    Test the PHP CS Fixer rules

--- php-cs-fixer-rules.php
+++ php-cs-fixer-rules.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 return [
     '@PSR12' => true,
     'binary_operator_spaces' => true,
+    'cast_spaces' => [
+        'space' => 'none',
+    ],
     'class_attributes_separation' => [
         'elements' => [
             'method' => 'one',
@@ -14,6 +17,11 @@ return [
         'closure_fn_spacing' => 'none',
     ],
     'function_typehint_space' => true,
+    'global_namespace_import' => [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
     'new_with_braces' => [
         'named_class' => true,
         'anonymous_class' => false,
@@ -28,6 +36,7 @@ return [
         ],
     ],
     'no_unused_imports' => true,
+    'nullable_type_declaration_for_default_null_value' => true,
     'ordered_imports' => [
         'imports_order' => ['class', 'function', 'const'],
         'sort_algorithm' => 'alpha',
@@ -35,6 +44,7 @@ return [
     'phpdoc_trim' => true,
     'php_unit_data_provider_static' => true,
     'single_space_after_construct' => true,
+    'strict_comparison' => true,
     'trailing_comma_in_multiline' => [
         'elements' => [
             'arrays',
