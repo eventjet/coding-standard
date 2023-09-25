fc8766c17b97dc46e9c43741a7ecfe9ab3a314fe
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2023-09-25 13:10:09 +0200

    PHP CS Fixer: Enable no_superfluous_phpdoc_tags

---
+++ tests/fixtures/invalid/unnecessary-param-annotation.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @param string $value
+ */
+function identity(string $value): string
+{
+    return $value;
+}
---
+++ tests/fixtures/invalid/unnecessary-return-annotation.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @return string
+ */
+function identity(string $value): string
+{
+    return $value;
+}
